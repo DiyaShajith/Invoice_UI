155fa1b3e0690c1747792e71387dd12a9f93f306
DiyaShajith <diyashajith.2231@gmail.com>
2024-08-01 11:35:47 +0530

    profilepage

--- lib/view/profile_view/profile.dart
+++ lib/view/profile_view/profile.dart
@@ -11,8 +11,8 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
-    return Scaffold(
-      appBar: AppBar(
+    return Scaffold(backgroundColor: Color(0XFF111318),
+      appBar: AppBar(backgroundColor: Color(0XFF111318),
         actions: [
           Icon(
             Icons.edit_calendar_outlined,
@@ -31,18 +31,91 @@ class _ProfileState extends State<Profile> {
             Stack(
               children: [
                 Container(
-                  width: size.width ,height: size.height*0.35,
+                  width: size.width,
+                  height: size.height * 0.35,
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(15),
-                      color: Colors.white),
-                ), Positioned(bottom: 0,left: 0,right: 0,
+                      ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
                   child: Container(
-                    width: size.width ,height: size.height*0.3,
+                    padding: EdgeInsets.all(12),
+                    width: size.width,
+
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(15),
-                        color: Colors.red),child: Column(children: [],),
+                        color: Color(0XFF0F0F0F)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [SizedBox(height: 20,),
+                        Text(
+                          "David",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
+                        Text(
+                          "david0012@gmail.com",
+                          style: TextStyle(
+                              color: Color(0XFFB5CDFE),
+                              fontWeight: FontWeight.w600,
+                              fontSize: 18),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Contact",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
+                        Text(
+                          "7894561230",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 18),
+                        ),
+                        SizedBox(
+                          height: 50,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton.icon(
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      WidgetStatePropertyAll(Colors.black)),
+                              icon: Icon(
+                                Icons.logout_outlined,
+                                color: Colors.red,
+                              ),
+                              onPressed: () {},
+                              label: Text(
+                                "Logout",
+                                style: TextStyle(
+                                    color: Colors.red,
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 16),
+                              )),
+                        )
+                      ],
+                    ),
                   ),
-                )
+                ), Align(alignment: Alignment.center,
+                  child: Container(
+                    width: 80,
+                    height: 80,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Color(0XFF111318),image: DecorationImage(image: AssetImage("assets/images/pngaaa 1.png"))),
+                  ),
+                ),
               ],
             )
           ],
